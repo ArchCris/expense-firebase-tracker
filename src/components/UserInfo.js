fab2452c5dcee20f54c3972fa997a609e537b330
arq.cristianmasci@gmail.com <arq.cristianmasci@gmail.com>
2023-09-20 23:33:00 +1000

    second

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,8 +1,30 @@
 import '../styles/UserInfo.css'
+import { useGetUserInfo } from '../hooks/useGetUserInfo' 
+import { signOut } from 'firebase/auth'
+import { auth } from '../config/firebase'
+import { useNavigate } from 'react-router-dom'
 
 const UserInfo = () => {
+
+  const navigate = useNavigate()
+
+  const {name, profilePhoto} = useGetUserInfo()
+
+  const handleLogout = async () => {
+    await signOut(auth)
+    localStorage.clear()
+    navigate('/')
+  }
+
+
   return (
-    <div className='userInfo__conteiner'>UserInfo</div>
+    <div className='userInfo__conteiner'>
+      <div className='userInfo__card'>
+        <img src={profilePhoto} alt='user logo'></img>
+        <h4>{name}</h4>
+      </div>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
   )
 }
 
